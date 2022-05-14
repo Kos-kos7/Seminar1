74148ec873b38a2b7ed6a83649be086532f30dd8
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-14 16:59:08 +0500

    Task_2 Про дни недели

---
+++ Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿//  будет выдавать название дня недели по заданному номеру
+Console.WriteLine("Введите число от 1 до 7");
+string numWeek = Console.ReadLine();
+if(numWeek == "1") Console.WriteLine("Понедельник");
+if(numWeek == "2") Console.WriteLine("Вторник");
+if(numWeek == "3") Console.WriteLine("Среда");
+if(numWeek == "4") Console.WriteLine("Четверг");
+if(numWeek == "5") Console.WriteLine("Пятница");
+if(numWeek == "6") Console.WriteLine("Суббота");
+if(numWeek == "7") Console.WriteLine("Воскресенье");
