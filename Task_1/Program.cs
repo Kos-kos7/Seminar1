9f9d16648a50394405e75ec348527f10d3a1b8d8
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-14 16:41:21 +0500

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Принимает 2 числа и проверяет является ли одно квадратом другого
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int numResult = a * a;
+
+Console.WriteLine(numResult);
+
+if(numResult == b) Console.WriteLine("Число b является квадратом числа a");
+elseConsole.WriteLine("Число b не является квадратом числа a");
