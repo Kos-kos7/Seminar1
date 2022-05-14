a757d2a9973fb4102b23e763287268fb059ce3b3
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-14 17:37:11 +0500

    HW2 Про обратное число

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает значение, а
+// на выходе показывает обратное значение.
+Console.WriteLine("Введите целое число =>");
+float x = int.Parse(Console.ReadLine());
+float y = 1 / x;
+Console.WriteLine(y);
